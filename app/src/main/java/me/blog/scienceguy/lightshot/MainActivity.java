2908a047e7ab5fc045b6d2bc56094b35badfeb09
juice500ml <juice500ml@gmail.com>
2016-02-12 18:21:20 +0900

    @Minhoryang comment fix Color.argb(color[0], color[1], color[2], color[3]) 이거 너무 많이쓰지않나여? --> made a new var.
    @Minhoryang comment fix
    OnTouchListener로 Class하나 만드시죠?ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
    아니면 Syntax Sugaring도!
    생성자로 i값을 받는게!
    --> Made a factory for listener

--- app/src/main/java/me/blog/scienceguy/lightshot/MainActivity.java
+++ app/src/main/java/me/blog/scienceguy/lightshot/MainActivity.java
@@ -18,7 +18,8 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    int[] color = {255,0,0,0};
+    // Alpha = 255 - color[0];
+    int[] color = {0,0,0,0};
     boolean[] isTouch = {false,false,false,false};
     private LinearLayout layout;
     private Button alphaB, redB, greenB, blueB, shotB;
@@ -30,22 +31,22 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-
-                        if(color[0]<255 && !isTouch[0]) color[0]++;
-                        for (int i = 1; i < 4; ++i) {
+                        for (int i = 0; i < 4; ++i) {
                             if (color[i] > 0 && !isTouch[i]) color[i]--;
                         }
 
-                        layout.setBackgroundColor(Color.argb(color[0], color[1], color[2], color[3]));
-                        alphaB.setText(String.format("Alpha\n%03d", color[0]));
-                        alphaB.setTextColor(Color.argb(255, 255 - color[1], 255 - color[2], 255 - color[3]));
+                        layout.setBackgroundColor(Color.argb(255-color[0], color[1], color[2], color[3]));
+                        alphaB.setText(String.format("Alpha\n%03d", 255 - color[0]));
                         redB.setText(String.format("Red\n%03d", color[1]));
-                        redB.setTextColor(Color.argb(255, 255 - color[1], 255 - color[2], 255 - color[3]));
                         greenB.setText(String.format("Green\n%03d", color[2]));
-                        greenB.setTextColor(Color.argb(255, 255 - color[1], 255 - color[2], 255 - color[3]));
                         blueB.setText(String.format("Blue\n%03d", color[3]));
-                        blueB.setTextColor(Color.argb(255, 255 - color[1], 255 - color[2], 255 - color[3]));
-                        shotB.setTextColor(Color.argb(255, 255 - color[1], 255 - color[2], 255 - color[3]));
+
+                        int textColor = Color.argb(255, 255-color[1], 255-color[2], 255-color[3]);
+                        alphaB.setTextColor(textColor);
+                        redB.setTextColor(textColor);
+                        greenB.setTextColor(textColor);
+                        blueB.setTextColor(textColor);
+                        shotB.setTextColor(textColor);
                     }
                 });
                 try {
@@ -69,46 +70,10 @@ public class MainActivity extends AppCompatActivity {
         blueB = (Button) this.findViewById(R.id.buttonBlue);
         shotB = (Button) this.findViewById(R.id.buttonShot);
 
-        alphaB.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                isTouch[0] = true;
-                if(event.getAction() == MotionEvent.ACTION_UP) isTouch[0] = false;
-                if(color[0]>0) color[0]--;
-                return false;
-            }
-        });
-
-        redB.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                isTouch[1] = true;
-                if(event.getAction() == MotionEvent.ACTION_UP) isTouch[1] = false;
-                if(color[1]<255) color[1]++;
-                return false;
-            }
-        });
-
-        greenB.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                isTouch[2] = true;
-                if(event.getAction() == MotionEvent.ACTION_UP) isTouch[2] = false;
-                if(color[2]<255) color[2]++;
-                return false;
-            }
-        });
-
-        blueB.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                isTouch[3] = true;
-                if (event.getAction() == MotionEvent.ACTION_UP) isTouch[3] = false;
-                if (color[3] < 255) color[3]++;
-                return false;
-            }
-        });
-
+        alphaB.setOnTouchListener(buttonListenerFactory(0));
+        redB.setOnTouchListener(buttonListenerFactory(1));
+        greenB.setOnTouchListener(buttonListenerFactory(2));
+        blueB.setOnTouchListener(buttonListenerFactory(3));
         shotB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -122,14 +87,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void takeScreenshot() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("(yyyy-MM-dd_hh:mm)");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("(yyyy-MM-dd hh:mm)");
 
         try {
             String mDir = Environment.getExternalStorageDirectory().toString() + "/LightShot/";
             (new File(mDir)).mkdir();
 
             // image naming and path  to include sd card  appending name you choose for file
-            String mPath = mDir + String.format("%03d-%03d-%03d-%03d ", color[0], color[1], color[2], color[3]) + dateFormat.format(new Date()) + ".jpg";
+            String mPath = mDir + String.format("%03d-%03d-%03d-%03d ", 255-color[0], color[1], color[2], color[3]) + dateFormat.format(new Date()) + ".jpg";
 
             // create bitmap screen capture
             View v1 = getWindow().getDecorView().getRootView();
@@ -150,5 +115,18 @@ public class MainActivity extends AppCompatActivity {
             // Several error may come out with file handling or OOM
             e.printStackTrace();
         }
+    }
+
+    private View.OnTouchListener buttonListenerFactory(final int colorIndex) {
+        View.OnTouchListener listener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                isTouch[colorIndex] = true;
+                if (event.getAction() == MotionEvent.ACTION_UP) isTouch[colorIndex] = false;
+                if (color[colorIndex] < 255) color[colorIndex]++;
+                return false;
+            }
+        };
+        return listener;
     }
 }
